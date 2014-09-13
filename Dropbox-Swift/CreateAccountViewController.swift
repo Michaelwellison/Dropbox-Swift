6587024627b8217da55fa4789e7f4cdc146946ae
Michael W. Ellison <michaelwellison@gmail.com>
2014-09-13 10:56:34 -0700

    First Commit

---
+++ Dropbox-Swift/CreateAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountViewController.swift
+//  Dropbox-Swift
+//
+//  Created by Michael Ellison on 9/12/14.
+//  Copyright (c) 2014 MichaelWEllison. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTapWelcomeButton(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    
+}
---
+++ Dropbox-Swift/SignInViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignInViewController.swift
+//  Dropbox-Swift
+//
+//  Created by Michael Ellison on 9/12/14.
+//  Copyright (c) 2014 MichaelWEllison. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTapWelcomeButton(sender: AnyObject) {
+    navigationController?.popViewControllerAnimated(true)
+    }
+
+}
