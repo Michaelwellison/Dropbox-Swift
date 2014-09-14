c1925b8fc32bade04b251011c472d6675cac0f64
Michael W. Ellison <michaelwellison@gmail.com>
2014-09-13 18:11:06 -0700

    Added subview to existing account photo section

---
+++ Dropbox-Swift/PhotoDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoDetailViewController.swift
+//  Dropbox-Swift
+//
+//  Created by Michael Ellison on 9/13/14.
+//  Copyright (c) 2014 MichaelWEllison. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBarController?.tabBar.hidden = true 
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
