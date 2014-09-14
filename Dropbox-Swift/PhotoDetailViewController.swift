ecfaf98c1449e074724ea4a886b8e2624f694262
Michael W. Ellison <michaelwellison@gmail.com>
2014-09-13 18:53:31 -0700

    User can now star in detail view

--- Dropbox-Swift/PhotoDetailViewController.swift
+++ Dropbox-Swift/PhotoDetailViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PhotoDetailViewController: UIViewController {
 
+    @IBOutlet weak var starButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,18 +22,17 @@ class PhotoDetailViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBAction func onTapStar(sender: AnyObject) {
+        
+        println("Button Pressed")
+        if starButton.selected {
+            starButton.selected = false
+        } else {
+            starButton.selected = true
+        }
+        
+    
+}
 }
